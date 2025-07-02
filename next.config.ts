c12a24b577542f620d31730d24456f97307cea40
Natan Gazola <nathanpereira.21@hotmail.com>
2025-07-01 21:34:08 -0300

    feat: update basePath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export'
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/bdstudio",
+  assetPrefix: "/bdstudio",
 };
 
 export default nextConfig;
